1e1447fae9d9da97821ad6ce3517e291084996bc
Jainil Vachhani <jainilvachhani@gmail.com>
2018-04-23 14:56:49 +0530

    blank

--- book_tickets/views.py
+++ book_tickets/views.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .models import Book,Theatre
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+import urllib
 # Create your views here.
 
-def post_list(request):
+
+def post_list(request, flag=False):
     theatre =  Theatre.objects.all()
-    return render(request, 'book_tickets/post_list.html', {'theatre':theatre})
+    #flag = False
+    return render(request, 'book_tickets/post_list.html', {'theatre':theatre, 'flag': flag})
     
 def add(request):
-    print("DDSS")
-    #query = Book.objects.filter(theatre_name = t,)
+    if request.method == 'POST':
+        theatre = request.POST.get('theatre')  
+        day = request.POST.get('day')
+        month = request.POST.get('month')
+        time = int (request.POST.get('time'))
+        d = '2018-' + month + '-' + day + ' 00:00:00'
+        query = Book.objects.filter(theatre_name = theatre, timeslot = time, date = d)
+        print("query")
+        if (query.exists()):
+            #kwargs['flag'] = True
+            flag = True
+            print("No cant's do")
+        else:
+            Book.objects.create(theatre_name = theatre, timeslot = time, date = d)
+        return redirect('post_list')
